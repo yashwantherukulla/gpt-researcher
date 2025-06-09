80c52b21d52bf8523fa8dd893bb012c15daf8934
yashwantherukulla <erukullayashwanth2005@gmail.com>
2025-06-09 12:31:13 +0530

    change: changed experimentation.py for faster runs

--- experimentation.py
+++ experimentation.py
@@ -4,17 +4,12 @@ import json
 
 async def get_report(query: str, config_dict: dict):
     researcher = GPTResearcher(query, config_dict=config_dict)
+
     print(json.dumps(researcher.cfg.__dict__, indent=2))
     
-    # await researcher.conduct_research()
-    # report = await researcher.write_report()
-    
-    # research_context = researcher.get_research_context()
-    # research_costs = researcher.get_costs()
-    # research_images = researcher.get_research_images()
-    # research_sources = researcher.get_research_sources()
-    
-    # return report, research_context, research_costs, research_images, research_sources
+    await researcher.conduct_research()
+    report = await researcher.write_report()
+    print(report)
 
 if __name__ == "__main__":
     query = "Should I invest in Nvidia?"
@@ -27,13 +22,3 @@ if __name__ == "__main__":
         "REPORT_SOURCE": "web"
     }
     asyncio.run(get_report(query, config_dict))
-    # report, context, costs, images, sources = asyncio.run(get_report(query, config_dict))
-    
-    # print("Report:")
-    # print(report)
-    # print("\nResearch Costs:")
-    # print(costs)
-    # print("\nResearch Images:")
-    # print(images)
-    # print("\nResearch Sources:")
-    # print(sources)
